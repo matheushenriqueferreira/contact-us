79966194debac13d0d7b38edb2c9fe712b36b6a5
MatheusHenriqueFerreira <maatheus27silva@gmail.com>
2022-08-01 21:34:27 -0300

    Func: Inserir tag div com msg ao fazer submit

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 window.onload = function() {
   const body = document.querySelector('body');
   const flexSwitch = document.querySelector('#flexSwitchCheckChecked');
-  const btnSend = document.querySelector("#btnSend");
+  const content = document.querySelector(".content");
+  const myDiv = document.createElement("div");
+  myDiv.setAttribute("class", "sendSucessMsg");
+  const mySpan = document.createElement("span");
+
+  const name = document.querySelector("#contactName");
 
   flexSwitch.addEventListener("change", (e) => {
     if(e.target.checked) {
@@ -10,6 +15,13 @@ window.onload = function() {
     else {
       body.className = 'light';
     }
-  })
+  });
+
+  document.querySelector("#contactForm").addEventListener("submit", (e) => {
+    e.preventDefault();//evita que o submit seja executado
+    content.appendChild(myDiv);
+    myDiv.appendChild(mySpan);
+    mySpan.innerHTML = `Olá <strong>${name.value}</strong>, sua mensagem foi enviada com sucesso!`;
+  });
 
 }
